c6d0c87b242f753cee58dfa32ab361a57ce5081f
haixi <64693535+fuhaixi@users.noreply.github.com>
2023-05-17 07:02:35 +0800

    update

--- DSTClusterConfig/mods/dedicated_server_mods_setup.lua
+++ DSTClusterConfig/mods/dedicated_server_mods_setup.lua
@@ -9,3 +9,21 @@
 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
 	--ServerModCollectionSetup("379114180")
+
+ServerModSetup("1694540893")
+ServerModSetup("1905387692")
+ServerModSetup("2032685784")
+ServerModSetup("2119742489")
+ServerModSetup("2506863549")
+ServerModSetup("2574469496")
+ServerModSetup("2694393579")
+ServerModSetup("2963262385")
+ServerModSetup("347079953")
+ServerModSetup("350811795")
+ServerModSetup("356420397")
+ServerModSetup("374550642")
+ServerModSetup("501385076")
+ServerModSetup("620716967")
+ServerModSetup("666155465")
+ServerModSetup("676503184")
+ServerModSetup("684098549")
